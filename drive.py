b4514c62f14284cab1c803d9f3ff43501a0b3a2e
NK ZOU <master.nk2010@gmail.com>
2017-02-15 22:49:27 -0800

    refactor: add preprocess function and tune param

--- drive.py
+++ drive.py
@@ -1,33 +1,14 @@
-import argparse
-import base64
-from datetime import datetime
-import os
-import shutil
-
-import numpy as np
+# 1- Function Set #########################################
 import socketio
-import eventlet
-import eventlet.wsgi
+import skimage.transform as sktransform
 from PIL import Image
-from flask import Flask
 from io import BytesIO
-from keras.models import load_model
-import h5py
-from keras import __version__ as keras_version
-import cv2
-
-# 1- #####################################################
-from keras.models import model_from_json
-import warnings
-import tensorflow as tf
-tf.python.control_flow_ops = tf
-# 1- ######################################################
+import base64
+import numpy as np
 
 sio = socketio.Server()
-app = Flask(__name__)
-model = None
-prev_image_array = None
 
+### Use preprocess function to resize image
 def preprocess(image, top_offset=.375, bottom_offset=.125):
     top = int(top_offset * image.shape[0])
     bottom = int(bottom_offset * image.shape[0])
@@ -36,109 +17,44 @@ def preprocess(image, top_offset=.375, bottom_offset=.125):
 
 @sio.on('telemetry')
 def telemetry(sid, data):
-#    if data:
-        # The current steering angle of the car
+### The current steering angle of the car
     steering_angle = data["steering_angle"]
-        # The current throttle of the car
+### The current throttle of the car
     throttle = data["throttle"]
-        # The current speed of the car
+### The current speed of the car
     speed = data["speed"]
-        # The current image from the center camera of the car
+### The current image from the center camera of the car
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
-    image_array = np.asarray(image)
-# 0- ######################################################
-    shape = image_array.shape
-    image_array = image_array[int(shape[0]/4):shape[0]-25, 0:shape[1]]
-    image_array = cv2.resize(image_array,(128, 32), interpolation=cv2.INTER_AREA)
-# 0- ######################################################
-	
-# 2- ######################################################
-    transformed_image_array = image_array[None, :, :, :]
-    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
-    throttle = .2 if float(speed) > 5 else 1.
-# 2- ######################################################	
-	
-#	steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
-        # Force the throttle = 0.2, make sure the vehicle keep running
-#        throttle = 0.2
-        #min_speed = 8
-        #max_speed = 10
-        #if float(speed) < min_speed:
-        #    throttle = 1.0
-        #elif float(speed) > max_speed:
-        #    throttle = -1.0
-        #else:
-        #    throttle = 0.1
+    image_array = preprocess(np.asarray(image))
+    steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+    throttle = .01 if abs(steering_angle) > 0.1 and float(speed) > 10 else .15  # else .5 during challenge model
     print(steering_angle, throttle)
     send_control(steering_angle, throttle)
-
-        # save frame
-#        if args.image_folder != '':
-#            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
-#            image_filename = os.path.join(args.image_folder, timestamp)
-#            image.save('{}.jpg'.format(image_filename))
-#    else:
-        # NOTE: DON'T EDIT THIS.
-#        sio.emit('manual', data={}, skip_sid=True)
-
 
 @sio.on('connect')
 def connect(sid, environ):
     print("connect ", sid)
     send_control(0, 0)
 
-
 def send_control(steering_angle, throttle):
-    sio.emit(
-        "steer",
-        data={
-            'steering_angle': steering_angle.__str__(),
-            'throttle': throttle.__str__()
-        },
-        skip_sid=True)
+    sio.emit("steer", data={'steering_angle': steering_angle.__str__(), 'throttle': throttle.__str__()}, skip_sid=True)
 
+# 2-Process ###############################################
+import argparse
+from keras.models import load_model
+from flask import Flask
+import eventlet
+import eventlet.wsgi
+
+model = None
+app = Flask(__name__)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Remote Driving')
-    parser.add_argument(
-        'model',
-        type=str,
-        help='Path to model h5 file. Model should be on the same path.'
-    )
-#    parser.add_argument(
-#        'image_folder',
-#        type=str,
-#        nargs='?',
-#        default='',
-#        help='Path to image folder. This is where the images from the run will be saved.'
-#    )
+    parser.add_argument('model', type=str, help='Path to model h5 file. Model should be on the same path.')
     args = parser.parse_args()
-
-    # check that model Keras version is same as local Keras version
-#    f = h5py.File(args.model, mode='r')
-#    model_version = f.attrs.get('keras_version')
-#    keras_version = str(keras_version).encode('utf8')
-
-#    if model_version != keras_version:
-#        print('You are using Keras version ', keras_version,
-#            ', but the model was built using ', model_version)
-        
     model = load_model(args.model)
-
-#    if args.image_folder != '':
-#        print("Creating image folder at {}".format(args.image_folder))
-#        if not os.path.exists(args.image_folder):
-#            os.makedirs(args.image_folder)
-#        else:
-#            shutil.rmtree(args.image_folder)
-#            os.makedirs(args.image_folder)
-#        print("RECORDING THIS RUN ...")
-#    else:
-#        print("NOT RECORDING THIS RUN ...")
-
-    # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
-
-    # deploy as an eventlet WSGI server
+### Deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
